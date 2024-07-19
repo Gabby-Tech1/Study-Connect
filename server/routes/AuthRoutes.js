91a7b01be5497f35ec236cc6f1cb25e983a1627d
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-07-19 18:12:17 +0000

    Create the backend server and implement the authentication session

---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { signup } from "../controllers/AuthControllers";
+
+
+const authRoutes = Router()
+
+authRoutes.post("/signup", signup)
+
+export default authRoutes
